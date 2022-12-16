6f1c02a35d91e9be975eeb0f71b84dc21dcc7b3f
Mads D. Pedersen <mdamgaard@biolib.com>
2022-12-16 13:56:23 +0100

    Change sample.fasta

--- predict.py
+++ predict.py
@@ -9,14 +9,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--fasta', default='input.csv')
 args = parser.parse_args()
 
-X = np.array(list(SeqIO.parse(args.fasta, "fasta")))
+X = [str(record.seq) for record in list(SeqIO.parse(args.fasta, "fasta"))]
 
 print(f"Running prediction on {len(X)} sequences")
 
 # Load model
-with open('', 'rb') as model_file:
+with open('model.pickle', 'rb') as model_file:
     model: tree.DecisionTreeRegressor = pickle.load(model_file)
-
 vectorize_on_length = np.vectorize(len)
 X_vectorized = np.reshape(vectorize_on_length(X), (-1, 1))
 predictions = model.predict(X_vectorized)
