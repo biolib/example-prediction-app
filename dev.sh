113ffb969165a9a34b08753bab95aa2935080ae6
Magnus Høie <mhoie@biolib.com>
2025-03-17 17:41:11 +0100

    fix

--- dev.sh
+++ dev.sh
@@ -2,10 +2,10 @@
 # File to run Docker container with mounted files!
 
 # Mount data folder to /data
-docker buildx build --platform linux/amd64 -t prediction-app .
+docker buildx build --platform linux/amd64 -t app-prediction .
 
 # Run interactively, starting a terminal
 docker run --rm -it \
     -v $(pwd)/predict.py:/home/biolib/predict.py \
     -v $(pwd)/data/:/home/biolib/data/ \
-    prediction-app /bin/bash
+    app-prediction /bin/bash
